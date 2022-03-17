f67519cb69e53876e46e8b3add7e4ff32241fbb8
linyuansup <linyuansup@gmail.com>
2022-03-17 20:21:25 +0800

    update

---
+++ queue.h
@@ -0,0 +1,84 @@
+#ifndef QUEUE_H_INCLUDED
+#define QUEUE_H_INCLUDED
+
+template<typename T>
+struct node
+{
+	T data;
+	node* next;
+};
+
+enum e_code
+{
+	overflow,
+	underflow,
+	success
+};
+
+template<class T>
+class queue
+{
+public:
+	queue();
+	~queue();
+	bool empty() const;
+	e_code get_front(T& x) const;
+	e_code append(T x);
+	e_code serve();
+
+private:
+	node<T>* front;
+	node<T>* rear;
+};
+
+#endif
+
+template<class T>
+queue<T>::queue()
+{
+	front = new node<T>;
+	front->next = NULL;
+	rear = front;
+}
+template<class T>
+bool queue<T>::empty() const
+{
+	return front == rear;
+}
+template<class T>
+e_code queue<T>::get_front(T& x) const
+{
+	if (empty())
+		return underflow;
+	x = front->next->data;
+	return success;
+}
+template<class T>
+e_code queue<T>::append(const T x)
+{
+	node<T>* s = new node<T>;
+	s->next = NULL;
+	s->data = x;
+	rear->next = s;
+	rear = s;
+	return success;
+}
+template<class T>
+e_code queue<T>::serve()
+{
+	if (empty())
+		return overflow;
+	node<T>* u = front->next;
+	front->next = u->next;
+	delete u;
+	if (front->next == NULL)
+		rear = front;
+	return success;
+}
+template<class T>
+queue<T>::~queue()
+{
+	while (!empty())
+		serve();
+	delete front;
+}
---
+++ sjjg/zy/5/t1.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include "stack.h"
+using namespace std;
+inline bool judge(char x)
+{
+	if (x == '+' || x == '-' || x == '*' || x == '/' || x == ')' || x == '#')
+		return true;
+	return false;
+}
+inline int calculate(int x, int y, char z)
+{
+	switch (z)
+	{
+	case '+':
+		return x + y;
+	case '-':
+		return x - y;
+	case '*':
+		return x * y;
+	case '/':
+		return x / y;
+	default:
+		return 0;
+	}
+}
+inline int priority(char x)
+{
+	switch (x)
+	{
+	case '#':
+		return 0;
+	case '(':
+	case ')':
+		return 1;
+	case '+':
+	case '-':
+		return 2;
+	case '*':
+	case '/':
+		return 3;
+	default:
+		return -1;
+	}
+}
+int main()
+{
+	stack<int> num;
+	stack<char> ope;
+	string s;
+	cin >> s;
+	int temp = 0;
+	ope.push(s[0]);
+	for (int i = 1; i < s.length(); i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			temp *= 10;
+			temp += s[i] - '0';
+			continue;
+		}
+		else if (s[i] == '(')
+			ope.push(s[i]);
+		else if (judge(s[i]))
+		{
+			num.push(temp);
+			temp = 0;
+			char a;
+			for (;;)
+			{
+				ope.get_top(a);
+				if (priority(a) < priority(s[i]))
+				{
+					ope.push(s[i]);
+					break;
+				}
+				else if (a == '#' && s[i] == '#')
+					break;
+				else
+				{
+					int b, c;
+					num.get_top(b);
+					num.pop();
+					num.get_top(c);
+					num.pop();
+					ope.pop();
+					if (a == '(')
+					{
+						ope.get_top(a);
+						ope.pop();
+						i++;
+					}
+					num.push(calculate(c, b, a));
+				}
+			}
+		}
+	}
+	int result;
+	num.get_top(result);
+	cout << result;
+	return 0;
+}
